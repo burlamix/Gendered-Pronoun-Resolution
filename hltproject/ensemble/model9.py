ae32093f3ff8a54a17caf41690b88e364836c316
Simone Spagnoli <simone.spagnoli@sas.com>
2019-10-26 11:08:17 +0200

    change file path

--- hltproject/ensemble/model9.py
+++ hltproject/ensemble/model9.py
@@ -127,8 +127,8 @@ if __name__ == "__main__":
 
     logger.info ("evaluating ")
     #val_probas_no_i_squad = model_squad_inst.evaluate( val_examples_df )
-    #val_probas_no_i_swag = model_swag_inst.evaluate( val_examples_df )
-    val_probas_no_i_SpanExtractor = model_SpanExtractor_inst.evaluate( test_path )
+    val_probas_no_i_swag = model_swag_inst.evaluate( val_examples_df )
+    #val_probas_no_i_SpanExtractor = model_SpanExtractor_inst.evaluate( test_path )
 
     print(val_probas_no_i_SpanExtractor)
 
